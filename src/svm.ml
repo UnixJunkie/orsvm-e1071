0905887749c7cd6d8984d57ef91424baba9de69c
Francois BERENGER <ligand@free.fr>
2018-02-21 16:59:39 +0900

    Svm: time for tests

--- src/svm.ml
+++ src/svm.ml
@@ -2,7 +2,7 @@ open Printf
 
 type filename = string
 
-let with_out_file (fn: filename) (f: out_channel -> 'a): 'a =
+let with_out_file fn f =
   let output = open_out_bin fn in
   let res = f output in
   close_out output;
@@ -16,7 +16,7 @@ let train
     (cost: float)
     (gamma: float)
     (model_fn: filename): bool =
-  (* create script *)
+  (* create R script *)
   let r_script =
     sprintf
       "library('e1071')\n\
@@ -28,14 +28,50 @@ let train
        save(model, file='%s')\n\
        quit()\n"
       data_fn labels_fn cost gamma model_fn in
-  (* dump it to tmp file *)
-  with_out_file model_fn (fun out ->
-      fprintf out "%s" r_script
-    );
+  (* dump it to file *)
+  with_out_file model_fn (fun out -> fprintf out "%s" r_script);
   (* execute it *)
   let cmd = sprintf "R --vanilla --slave < %s 2>&1 > /dev/null" r_script in
-  (0 = Sys.command cmd)
+  (Sys.command cmd = 0)
 
-(* use model in 'model_fn' to predict decision values for test data in 'data_fn' *)
-let predict (model_fn: filename) (data_fn: filename): float array =
-  failwith "not implemented yet"
+(* use model in 'model_fn' to predict decision values for test data in 'data_fn'.
+   the exit code of the R script is returned *)
+let predict
+    (model_fn: filename)
+    (data_fn: filename)
+    (predictions_fn: filename): bool =
+  (* create R script *)
+  let r_script =
+    sprintf
+      "newdata = as.matrix(read.table('%s'))\n\
+       load('%s')\n\
+       values = attributes(predict(model, newdata, decision.values = TRUE)\
+                          )$decision.values\n\
+       write.table(values, file = '%s', sep = '\n', \
+                   row.names = FALSE, col.names = FALSE)\n\
+       quit()\n"
+      data_fn model_fn predictions_fn in
+  (* dump it to temp file *)
+  let r_script_fn = Filename.temp_file "orsvm_e1071_" ".r" in
+  with_out_file r_script_fn (fun out -> fprintf out "%s" r_script);
+  (* execute it *)
+  let cmd = sprintf "R --vanilla --slave < %s 2>&1 > /dev/null" r_script_fn in
+  let exit_status = Sys.command cmd in
+  Sys.remove r_script_fn; (* rm temp file *)
+  (exit_status = 0)
+
+let iter_on_lines_of_file fn f =
+  let input = open_in_bin fn in
+  try
+    while true do
+      f (input_line input)
+    done
+  with End_of_file -> close_in input
+
+(* read the predicted decision values from 'predictions_fn' *)
+let read_predictions (predictions_fn: filename): float list =
+  let res = ref [] in
+  iter_on_lines_of_file predictions_fn (fun line ->
+      Scanf.sscanf line "%f" (fun x -> x)
+    );
+  List.rev !res
