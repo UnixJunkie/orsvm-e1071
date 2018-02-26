b8f7dff34e1c0afd9376aa4a91b88f85e05f6faa
Francois BERENGER <ligand@free.fr>
2018-02-26 12:04:59 +0900

    Svmpath: factorized out some things to Utls

--- src/svmpath.ml
+++ src/svmpath.ml
@@ -2,25 +2,11 @@ open Printf
 
 module L = List
 
-module Utls = struct
-  #include "utls.ml"
-end
-
 type filename = string
 
 (* capture everything in case of error *)
-(* FBR: factorize this out *)
-let collect_script_and_log
-    (r_script_fn: filename) (r_log_fn: filename) (model_fn: filename)
-  : Result.t =
-  let buff = Buffer.create 4096 in
-  bprintf buff "--- %s ---\n" r_script_fn;
-  Utls.append_file_to_buffer buff r_script_fn;
-  bprintf buff "--- %s ---\n" r_log_fn;
-  Utls.append_file_to_buffer buff r_log_fn;
-  let err_msg = Buffer.contents buff in
-  L.iter Sys.remove [r_script_fn; r_log_fn; model_fn];
-  Error err_msg
+let collect_script_and_log =
+  Utls.collect_script_and_log
 
 (* train a SVM with linear kernel and find all values of lambda *)
 let train ?debug:(debug = false)
@@ -111,9 +97,6 @@ let predict ?debug:(debug = false) ~lambda:lambda
         (if not debug then L.iter Sys.remove [r_script_fn; r_log_fn])
         (Result.Ok predictions_fn)
 
-(* read the predicted decision values *)
-(* FBR: factorize this out *)
-let read_predictions (maybe_predictions_fn: Result.t): float list =
-  match maybe_predictions_fn with
-  | Error err -> failwith err (* should have been handled by user before *)
-  | Ok predictions_fn -> Utls.float_list_of_file predictions_fn
+(* read predicted decision values *)
+let read_predictions =
+  Utls.read_predictions
